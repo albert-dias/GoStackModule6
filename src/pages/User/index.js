02b27ab5bb90e985941243bda8c4738a9d283b56
Albert-dias <albert.uft@gmail.com>
2020-03-09 21:49:49 -0300

    finalization

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import PropTypes from 'prop-types';
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+    Container,
+    Header,
+    Avatar,
+    Name,
+    Bio,
+    Stars,
+    Starred,
+    OwnerAvatar,
+    Info,
+    Title,
+    Author,
+} from './styles';
 
 export default class User extends Component {
+    static propTypes = {
+        navigation: PropTypes.shape({
+            setOptions: PropTypes.func,
+        }).isRequired,
+        route: PropTypes.shape().isRequired,
+    };
+
     state = {
         stars: [],
     };
@@ -20,6 +39,32 @@ export default class User extends Component {
     }
 
     render() {
-        return <Text>Teste</Text>;
+        const { route } = this.props;
+        const { stars } = this.state;
+        const { user } = route.params;
+        return (
+            <Container>
+                <Header>
+                    <Avatar source={{ uri: user.avatar }} />
+                    <Name>{user.name}</Name>
+                    <Bio>{user.bio}</Bio>
+                </Header>
+                <Stars
+                    data={stars}
+                    keyExtractor={star => String(star.id)}
+                    renderItem={({ item }) => (
+                        <Starred>
+                            <OwnerAvatar
+                                source={{ uri: item.owner.avatar_url }}
+                            />
+                            <Info>
+                                <Title>{item.name}</Title>
+                                <Author>{item.owner.login}</Author>
+                            </Info>
+                        </Starred>
+                    )}
+                />
+            </Container>
+        );
     }
 }
